02fc9cbfbbe25001c5c8d4aa613ffe8f79ebb61e
Sharma <harshit.b.sharma@accenture.com>
2019-10-09 15:27:25 +0530

    Css Commit

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 
 const app=express();
 
-const port=8000  //const port=1021 
+//const port=8000  
+const port=1021 
+
+app.use(express.static('./assets'))
 
 const expressLayout=require('express-ejs-layouts')
 
