11a5981711b2c99ed3fade253fe5c6f16470affd
harry981 <sharmahs981@gmail.com>
2019-10-15 23:05:06 +0530

    manual local auth Sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,23 @@
+const Users=require('../models/user')
+
 module.exports.profile=function(req,res){
-        return res.render('user_profile',{title:"Users"})
+        if(req.cookies.user_id)
+        {     
+                Users.findById(req.cookies.user_id,function(err,user){
+                      if(user)
+                      {
+                        return res.render('user_profile',{title:user.name,user:user})
+                      }
+                      else
+                      {
+                              return res.redirect('/signin')
+                      }
+                })
+        
+              
+        }
+        else
+        {
+              return res.redirect('/signin')
+        }
 }
