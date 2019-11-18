a45d1ac0df4e1685bf8259fb3ab76cc726972bc3
harry981 <sharmahs981@gmail.com>
2019-11-18 18:50:03 +0530

    Comments Schema

--- models/post.js
+++ models/post.js
@@ -9,6 +9,13 @@ const postSchema=new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
+    //include the array of ids of all comments in this post schema itself so that we can find the comments quickly for a post
+    comments:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Comment'
+        }
+    ]
 },
     {
         timestamps:true
--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,12 @@ const express=require('express')
 
 const router=express.Router();
 
+const passport = require('passport')
+
 const postController=require('../controllers/postController')
 
 
-router.post('/create',postController.create);
+router.post('/create', passport.checkAuthentication,postController.create);
 
 
 module.exports=router;
