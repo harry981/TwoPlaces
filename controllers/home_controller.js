db2cd92538e93d1e2b6f805939fc1f663ca4fc20
harry981 <sharmahs981@gmail.com>
2019-11-18 17:50:24 +0530

    Populate User for Post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,19 @@
+const Post=require('../models/post')
+
 module.exports.home=function(req,res){
-      console.log(req.cookies)
-      res.cookie('user_id',25);
-      return res.render('home',{title:"Wooohoooo !!!"})
+      // console.log(req.cookies)
+      // res.cookie('user_id',25);
+      // Post.find({},function(err,posts){
+      //       return res.render('home',{
+      //       title:"Social Development  | Posts ",
+      //       posts:posts
+      //       })      
+      // })
+      //populate the user of each post
+      Post.find({}).populate('user').exec(function(err,post){
+           return res.render('home',{
+                 title:'Social Development | Codial',
+                 posts:post
+           }) 
+      })
 }
