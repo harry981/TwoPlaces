f42f32887da3b03bfb90dc1a24bdd56123813979
Sharma <harshit.b.sharma@accenture.com>
2019-11-19 19:05:21 +0530

    Comments Fetch and Display

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,7 +10,15 @@ module.exports.home=function(req,res){
       //       })      
       // })
       //populate the user of each post
-      Post.find({}).populate('user').exec(function(err,post){
+      Post.find({})
+      .populate('user')
+      .populate({
+            path:'comments',
+            populate:{
+                  path:'user'
+            }
+      })
+      .exec(function(err,post){
            return res.render('home',{
                  title:'Social Development | Codial',
                  posts:post
