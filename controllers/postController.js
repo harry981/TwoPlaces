51221bdfce37126dbdeaaa8f16dc287141eb5dd6
harry981 <sharmahs981@gmail.com>
2019-11-24 17:14:45 +0530

    Async/Await added more files

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,32 +2,50 @@ const Post=require('../models/post')
 
 const Comment = require('../models/comment')
 
-module.exports.create= function(req,res){
-    Post.create({
+module.exports.create=async function(req,res){
+   try{
+     
+    await Post.create({
         content:req.body.content,
         user:req.user._id
-    },function(err,post){
-        if(err){
-            console.log('Unable in creating a post in Mongo')
-            return;
-        }
-
-        return res.redirect('back');
     })
+    return res.redirect('back');
+   }
+   catch(err)
+   {
+      console.log(err)
+      return;
+   }
+
+
 }
 
-module.exports.destroy=function(req,res){
-   Post.findById(req.params.id,function(err,post){
-    //.id means converting the object id to string   
+// module.exports.destroy=function(req,res){
+//    Post.findById(req.params.id,function(err,post){
+//     //.id means converting the object id to string   
+//     if(post.user==req.user.id){
+//         post.remove();
+
+//         Comment.deleteMany({post:req.params.id},function(err){
+//             return res.redirect('back')
+//         })
+//     }
+//     else{
+//         return res.redirect('back')
+//     }
+//    })
+// }
+
+module.exports.destroy=async function(req,res){
+    let post=await Post.findById(req.params.id)
+
     if(post.user==req.user.id){
         post.remove();
 
-        Comment.deleteMany({post:req.params.id},function(err){
-            return res.redirect('back')
-        })
+        await Comment.deleteMany({post:req.params.id})
+        return res.redirect('back')
     }
     else{
         return res.redirect('back')
     }
-   })
-}
+ }
