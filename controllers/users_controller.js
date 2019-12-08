4efc0fb0e04888cd6670fc8e267dcbdff3522b32
harry981 <sharmahs981@gmail.com>
2019-12-08 18:08:56 +0530

    Delete User Image

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User=require('../models/user')
+const fs=require('fs')
+const path=require('path')
 
 module.exports.profile=function(req,res){
 User.findById(req.params.id,function(err,user){
@@ -35,6 +37,10 @@ module.exports.update=async function(req,res){
                              user.name=req.body.name;
                              user.email=req.body.email
                              if(req.file){
+
+                                if(user.avatar){
+                                     fs.unlinkSync(path.join(__dirname,'..',user.avatar))
+                                }
                                      //this is saving the path of the uploaded file into the avatar field of the user
                                      user.avatar=User.avatarPath+'/'+req.file.filename
                              }
